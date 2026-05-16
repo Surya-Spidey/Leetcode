d4ba4926f076c1f39c649af3eac2b4af6c2368ad
Surya <suryaunder18@gmail.com>
2026-05-16 23:12:49 +0530

    Code Updation

--- largestnum.js
+++ largestnum.js
@@ -1,8 +1,11 @@
 let num = [1, 2, 3, 4, 5];
-let lar = num[0];
-for (let i = 0; i < num.length; i++) {
+function largest(num){
+    let lar = num[0];
+    for (let i = 0; i < num.length; i++) {
     if (num[i] > lar) {
         lar = num[i];
     }
 }
-console.log(lar);  
+return lar;     
+}
+let result = largest(num);
