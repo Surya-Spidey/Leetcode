83b04d4f001f75832c81b7c551de5f8d31db6bcd
Surya <suryaunder18@gmail.com>
2026-05-18 23:24:54 +0530

    Update

--- Sample/angela.js
+++ Sample/angela.js
@@ -1,3 +1,21 @@
 import {randomSuperhero} from 'superheroes';
 const name = randomSuperhero();
 console.log(name);
+
+import fs from 'fs';
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import { text } from 'stream/consumers';
+
+inquirer
+    .prompt([
+        { message : 'Enter the text to generate QR code', name: 'text' },
+    ])
+    .then((answers) => {
+        const url = answers.text
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('url.jpg'));
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
