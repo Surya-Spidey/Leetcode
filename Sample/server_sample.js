cf30eb0e7832dcf29325e97d4e8b1a52148c0090
Surya <suryaunder18@gmail.com>
2026-06-01 22:51:39 +0530

    Sample Contributional Commit

--- Sample/server_sample.js
+++ Sample/server_sample.js
@@ -8,3 +8,11 @@ app.get("/data", (req, res) => {
 app.listen(5500, () => {
     console.log("Server is running on port 5500");
 });
+
+app.push("/data", (req, res) => {
+    res.send("This is a push request");
+});
+
+app.options("/data", (req, res) => {
+    res.send("This is an options request");
+});
