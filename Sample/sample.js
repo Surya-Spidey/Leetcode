5ecd32d0aa7dc3a12c7372d7ba3ec66212671d95
Surya <suryaunder18@gmail.com>
2026-06-18 22:50:19 +0530

    Body Parser

--- Sample/sample.js
+++ Sample/sample.js
@@ -4,10 +4,12 @@
 //     console.log(data);
 // }
 
-
+import bodyParser from 'body-parser';
 import express from 'express';
 const app =express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get("/",(req,res) => {
     res.send('Hello World!');
