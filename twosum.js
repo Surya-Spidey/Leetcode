2a52954210bef57f551d60d7f98c292fafc5a31c
Surya <suryaunder18@gmail.com>
2026-06-28 01:41:11 +0530

    Replenished code

--- twosum.js
+++ twosum.js
@@ -1,13 +1,23 @@
 let num = [1,2,3,4,5];
-target = 5;
+const target = 5;
 var twoSum = function(nums, target) {
     let map = new Map();
-    for(let i = 0; i < nums.length; i++){
-        let need = target -nums[i];
-        if(map.has(need)){
-            return [map.get(need), i];
-        }        map.set(nums[i], i); 
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        let need = target - nums[i];
+        if (map.has(need)) {
+            let indices = map.get(need);
+            for (let index of indices) {
+                result.push([index, i]);
+            }
+        }
+
+        if (!map.has(nums[i])) {
+            map.set(nums[i], []);
+        }
+        map.get(nums[i]).push(i);
     }
+    return result;
 };
 
 console.log(twoSum(num, target));
@@ -17,12 +27,12 @@ console.log(twoSum(num, target));
 
 let nums2 = [1,2,3,4,5];
 target2 = 5;
-let result = [];
+let result2 = [];
 var twoSum2 = function(nums2, target2) {
     for (let i = 0; i < nums2.length; i++)
         for (let j = i + 1; j < nums2.length; j++)
             if (nums2[i] + nums2[j] === target2) 
-                result.push(i,j);
-    return result;
+                result2.push(i,j);
+    return result2 ;
 }
 console.log(twoSum2(nums2, target2));
